4c2e432a256f6f31b33c35d2c21ad514fec62ddf
Abby Brown <abby_brown3@baylor.edu>
2021-02-11 18:43:22 -0600

    Working on HW 2/11

---
+++ retail_program.py
@@ -0,0 +1,7 @@
+import oop_hw as oop
+
+record1 = oop.RetailItem("Susan Meyers", 47899, "Accounting", "Vice President")
+
+record2 = oop.RetailItem("Mark Jones", 39119, "IT", "Programmer")
+
+record3 = oop.RetailItem("Joy Rogers", 81774, "Manufacturing", "Engineering")
